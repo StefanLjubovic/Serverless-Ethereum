de3a69ca615ecc4f8c115820b2cfdf9c01575150
Stefan Ljubovic <stefan.ljubovic@levi9.com>
2023-06-15 15:03:57 +0200

    Added nft smart contract

--- frontend/src/components/EnrolledCourse/EnrolledLeft/EnrolledLeft.jsx
+++ frontend/src/components/EnrolledCourse/EnrolledLeft/EnrolledLeft.jsx
@@ -1,10 +1,21 @@
 import "./EnrolledLeft.css"
-import { Player } from 'react-html5video';
-import 'react-html5video/dist/styles.css';
+import { DefaultPlayer as Video } from 'react-html5video';
+import videtoTest from "../../../assets/videoplayback.mp4"
+import 'react-html5video/dist/styles.css'
+import Img from "../../../assets/code.jpg"
 function EnrolledLeft(){
     return(
         <div className="e-left">
-        
+            <div>
+            <Video autoplay loop poster={Img}
+                onCanPlayThrough={()=>{
+                    console.log("video play")
+                }}
+                style={{ width: "100%", height: "35rem" }}
+            >
+                <source src={videtoTest} type="video/webm"/>
+            </Video>
+            </div>
         </div>
     );
 }
